bdeb8fb8877f5b0fc3ec93cd1a8381c31d96c9e7
Christian Schwartz <christian@schwartz-raum.de>
2011-08-24 11:30:55 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyLittleJobQueue::Application.config.secret_token = '9d8d3d331b17a5ae7d6d0ee64fad878b9791598ef7631a2e35badc2e2545cbf33ee595566f07a433830f13a7418fad99b9e97d2418159f3002f6792144db42a2'
